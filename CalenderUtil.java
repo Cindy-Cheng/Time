294db4f287d44ee92235a248152fd5968abd5eab
Cindy-Cheng <1639270823@qq.com>
2016-06-03 21:28:29 +0800

    add time different

---
+++ CalenderUtil.java
@@ -0,0 +1,30 @@
+package com.dy.cy.test3;
+import static java.lang.System.out;
+import java.util.Calendar;
+public class CalenderUtil {
+	public static void main(String[] args){
+		Calendar birth = Calendar.getInstance();
+		birth.set(1975,Calendar.MAY,26);
+		Calendar now = Calendar.getInstance();
+		out.printf("ËêÊı:",yearsBetween(birth,now));
+		out.printf("ÌìÊı:",yearsBetween(birth,now));
+	}
+	public static long yearsBetween(Calendar begin,Calendar end){
+		Calendar calendar = (Calendar) begin.clone();
+		long years = 0;
+		while(calendar.before(end)){
+			calendar.add(Calendar.YEAR, 1);
+			years++;
+		}
+		return years - 1;
+	}
+	public static long dayBetween(Calendar begin,Calendar end){
+		Calendar calendar = (Calendar) begin.clone();
+		long days = 0;
+		while(calendar.before(end)){
+			calendar.add(Calendar.DATE,1);
+			days++;
+		}
+		return days - 1;
+	}
+}
